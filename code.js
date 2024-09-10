8d672aa929e42d6a2afb3fcfbf1ec72182d32f2c
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-10 12:42:13 -0600

    Add files via upload

--- code.js
+++ code.js
@@ -1,29 +1,48 @@
-function quicksort(array) {
-    let stack = [0,array.length - 1];
-    while (stack.length > 0)
-    {
-        var i,p;
-        let hi = stack.pop();
-        let lo = stack.pop();
-        if (lo >= hi) continue;
-        p = lo;
-
-        for (i = lo + 1; i <= hi; i++) {
-            if (array[i] < array[lo]) swap(array, ++p, i);
-        }
-        
-        swap(array, p, lo);
-
-        stack.push(lo);
-        stack.push(p - 1);
-        stack.push(p + 1);
-        stack.push(hi);
-    }
-    return array;
+function fib(n) {
+    if (n === 0) return [0];
+    if (n === 1) return [0,1];
+    var arr = [0, 1];
+    fibHelper(arr,n);
+    return arr;
 }
 
-function swap(array, i, j) {
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+function fibHelper(arr, max) {
+    var len = arr.length;
+    if (len === max + 1) return arr;
+    var len = arr.length;
+    arr.push(arr[len - 1] + arr[len - 2]);
+    fibHelper(arr, max);
+}
+
+// console.log(fib(0));
+
+const fs = require('fs');
+const jsc = require('jsverify');
+
+// eval(fs.readFileSync('code.js')+'');
+
+function fibTest(n) {
+    var fib_solns = [0];
+    if(n == 0) return fib_solns;
+    fib_solns[1] = 1;
+    if(n == 1) return fib_solns;
+    for(var i = 2; i <= n; i++) fib_solns[i] =
+        fib_solns[i-1] + fib_solns[i-2];
+    return fib_solns;
+}
+
+const test =
+    jsc.forall("nat", function(n) {
+        return JSON.stringify(fib(n)) ==
+            JSON.stringify(fibTest(n));
+    });
+
+// jsc.assert(test);
+
+// Print the result
+try {
+    jsc.assert(test);
+    console.log("Test passed");
+} catch (error) {
+    console.log("Test failed:", error.message);
 }
