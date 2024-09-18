bc109e37a32fcc2dba283d3da786f2ebc88737cb
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-18 16:18:31 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
 function quicksort(array) {
+    let stack = [0, array.length - 1];
+    while (stack.length > 0) {
+        let hi = stack.pop();
+        let lo = stack.pop();
+        if (lo >= hi) continue;
+        let p = lo;
+        
+        for (let i = lo + 1; i <= hi; i++) {
+            if (array[i] < array[lo]) swap(array, ++p, i);
+        }
+        
+        swap(array, p, lo);
+        
+        stack.push(lo);
+        stack.push(p - 1);
+        stack.push(p + 1);
+        stack.push(hi);
+    }
     return array;
+}
+
+function swap(array, i, j) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
 }
